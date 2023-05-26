33ee18c84f7aaec1248034f89d2c0ec45afab573
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2023-05-26 14:56:52 -0300

    Update Week7

--- README.md
+++
@@ -1,2 +0,0 @@
-# Harvard-CS50
-https://cs50.harvard.edu/x/2023/
--- week_7.py
+++
@@ -1,7 +0,0 @@
-# Harvard CS50
-# https://cs50.harvard.edu/x/2023/
-
-# Week 7 SQL
-# https://cs50.harvard.edu/x/2023/weeks/7/
-# tags: SQL: Tables; Types; Statements; Constraints; Indexes; Keywords, Functions; Transactions. Race Conditionals. SQL Injection Attacks.
-
